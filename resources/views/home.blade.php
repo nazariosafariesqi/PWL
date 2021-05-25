d80914b14a26ccdf1429f84c14ef028991797034
nazariosafariesqi <nazariotyo@gmail.com>
2021-05-26 02:45:43 +0700

    Update Controller & View

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="landingPage.css" rel="stylesheet" />
+    <link href="css/landingPage.css" rel="stylesheet" />
 </head>
 
 <body id="page-top">
@@ -278,7 +278,7 @@
     <!-- Third party plugin JS-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
     <!-- Core theme JS-->
-    <script src="landingPage.js"></script>
+    <script src="js/landingPage.js"></script>
 </body>
 
 </html>
